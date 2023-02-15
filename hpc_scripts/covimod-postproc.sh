26d8a314f25eb14baf01b39922e6b3d35d0573e3
joshuategegne1 <57180120+joshuategegne1@users.noreply.github.com>
2023-02-15 09:01:19 +0000

    Update covimod-postproc.sh

--- hpc_scripts/covimod-postproc.sh
+++ hpc_scripts/covimod-postproc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #PBS -l walltime=23:59:59
-#PBS -l select=1:ncpus=8:mem=64gb
+#PBS -l select=1:ncpus=4:mem=64gb
 
 REPO_PATH=/rds/general/user/jt1319/home/bayes-rate-consistency
-MODEL="hsgp-eq-lrd"
+MODEL="hsgp-eq-lrd-cnst"
 WAVES=5
 MIXING=TRUE
 PPC=TRUE
