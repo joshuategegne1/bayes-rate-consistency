47be4b90b0c98f3296972c46e1dbbdeed0614397
joshuategegne1 <57180120+joshuategegne1@users.noreply.github.com>
2023-02-18 13:09:24 +0000

    Update covimod-cross-fullproc.sh

--- hpc_scripts/covimod-cross-fullproc.sh
+++ hpc_scripts/covimod-cross-fullproc.sh
@@ -1,26 +1,12 @@
 #!/bin/sh
 
-#PBS -l walltime=08:00:00
+#PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=10:ompthreads=1:mem=100gb
 
-REPO_PATH=/rds/general/user/sd121/home/covimod-gp
-WAVE=5
-MODEL="rd-hsgp"
-HSGP_C=1.5
-HSGP_M=20
-
-# HMC Sampler params
-CHAINS=8
-WARMUP=500
-SAMPLING=1000
-
-# Post-processing
-MIXING=TRUE
+REPO_PATH=/rds/general/user/jt1319/home/bayes-rate-consistency
+WAVES=5
 
 module load anaconda3/personal
 source activate Renv
 
-Rscript $REPO_PATH/scripts/run-stan-single.R --wave $WAVE --model $MODEL --hsgp_c $HSGP_C --hsgp_m $HSGP_M --chains $CHAINS --iter_warmup $WARMUP --iter_sampling $SAMPLING
-
-MODEL=${MODEL}-${WAVE}
-Rscript $REPO_PATH/scripts/postprocess-single.R --model $MODEL --mixing $MIXING
+Rscript $REPO_PATH/scripts/loo-compare.R --waves $WAVES
