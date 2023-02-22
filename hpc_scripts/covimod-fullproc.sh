1e0cd2dc763fdeba10c56dd53d8fbad2a55ce4ff
joshuategegne1 <57180120+joshuategegne1@users.noreply.github.com>
2023-02-22 15:36:46 +0000

    Update covimod-fullproc.sh

--- hpc_scripts/covimod-fullproc.sh
+++ hpc_scripts/covimod-fullproc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #PBS -l walltime=56:00:00
-#PBS -l select=1:ncpus=10:ompthreads=1:mem=100gb
+#PBS -l select=1:ncpus=10:ompthreads=1:mem=300gb
 
 REPO_PATH=/rds/general/user/jt1319/home/bayes-rate-consistency
 WAVES=5
