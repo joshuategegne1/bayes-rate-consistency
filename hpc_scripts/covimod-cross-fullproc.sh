78483ece2303ac9a1dd3eb7985076292d6fab5f1
joshuategegne1 <57180120+joshuategegne1@users.noreply.github.com>
2023-05-17 12:06:59 +0100

    Update covimod-cross-fullproc.sh

--- hpc_scripts/covimod-cross-fullproc.sh
+++ hpc_scripts/covimod-cross-fullproc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=10:ompthreads=1:mem=300gb
+#PBS -l select=1:ncpus=10:ompthreads=1:mem=301gb
 
 REPO_PATH=/rds/general/user/jt1319/home/bayes-rate-consistency
 WAVES=5
