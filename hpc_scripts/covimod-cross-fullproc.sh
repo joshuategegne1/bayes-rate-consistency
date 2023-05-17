9ce86376b23b558ad400135dd4a2ada24be1f611
joshuategegne1 <57180120+joshuategegne1@users.noreply.github.com>
2023-05-17 10:41:57 +0100

    Update covimod-cross-fullproc.sh

--- hpc_scripts/covimod-cross-fullproc.sh
+++ hpc_scripts/covimod-cross-fullproc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#PBS -l walltime=08:00:00
-#PBS -l select=1:ncpus=10:ompthreads=1:mem=400gb
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=4:ompthreads=1:mem=500gb
 
 REPO_PATH=/rds/general/user/jt1319/home/bayes-rate-consistency
 WAVES=5
@@ -11,4 +11,4 @@ MODEL=${MODEL}-${WAVES}
 module load anaconda3/personal
 source activate Renv
 
-Rscript $REPO_PATH/scripts/rep-fatigue.R
+Rscript $REPO_PATH/scripts/loo-compare.R
