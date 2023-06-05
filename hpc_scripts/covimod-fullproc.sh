3f5e31e711ffb15e513bc907e6273b792e855634
joshuategegne1 <57180120+joshuategegne1@users.noreply.github.com>
2023-06-05 15:13:38 +0100

    Update covimod-fullproc.sh

--- hpc_scripts/covimod-fullproc.sh
+++ hpc_scripts/covimod-fullproc.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 #PBS -l walltime=72:00:00
-#PBS -l select=1:ncpus=10:ompthreads=1:mem=100gb
+#PBS -l select=1:ncpus=10:ompthreads=1:mem=150gb
 
 REPO_PATH=/rds/general/user/jt1319/home/bayes-rate-consistency
 WAVES=5
-MODEL="hsgp-eq-lrd-gp"
+MODEL="hsgp-eq-lrd-none"
 HSGP_C=1.5
 HSGP_M1=30
 HSGP_M2=30
@@ -15,10 +15,14 @@ CHAINS=8
 WARMUP=500
 SAMPLING=500
 
+MIXING=TRUE
+PPC=TRUE
+PLOT=TRUE
+
 module load anaconda3/personal
 source activate Renv
 
 Rscript $REPO_PATH/scripts/run-stan.R --waves $WAVES --model $MODEL --hsgp_c $HSGP_C --hsgp_m1 $HSGP_M1 --hsgp_m2 $HSGP_M2 --chains $CHAINS --iter_warmup $WARMUP --iter_sampling $SAMPLING
 
 MODEL=${MODEL}-${WAVES}
-Rscript $REPO_PATH/scripts/postprocess.R --model $MODEL
+Rscript $REPO_PATH/scripts/postprocess.R --model $MODEL --model $MODEL --mixing $MIXING --ppc $PPC --plot $PLOT
